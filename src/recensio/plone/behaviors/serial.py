f922587aeb7ec3ea030a1372e08af8362b16e360
ale-rt <alessandro.pisa@gmail.com>
2023-01-20 14:48:06 +0100

    Serial behavior

---
+++ src/recensio/plone/behaviors/serial.py
@@ -0,0 +1,45 @@
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.dexterity.interfaces import IDexterityContent
+from plone.supermodel import model
+from recensio.plone import _
+from zope import schema
+from zope.component import adapter
+from zope.interface import provider
+
+
+@provider(IFormFieldProvider)
+class ISerial(model.Schema):
+
+    series = schema.TextLine(
+        title=_("Series"),
+        required=False,
+    )
+
+    seriesVol = schema.TextLine(
+        title=_("Series (vol.)"),
+        required=False,
+    )
+
+
+@adapter(IDexterityContent)
+class Serial:
+    """Adapter for ISeries"""
+
+    def __init__(self, context):
+        self.context = context
+
+    @property
+    def series(self):
+        return self.context.series
+
+    @series.setter
+    def series(self, value):
+        self.context.series = value
+
+    @property
+    def seriesVol(self):
+        return self.context.seriesVol
+
+    @seriesVol.setter
+    def seriesVol(self, value):
+        self.context.seriesVol = value
