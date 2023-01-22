8b2b4d986c4eb528ccc057b5656a615848a05fff
Johannes Raggam <thetetet@gmail.com>
2023-01-22 12:36:31 +0100

    CT: Implement "Review Monograph".

--- src/recensio/plone/content/review_monograph.py
+++ src/recensio/plone/content/review_monograph.py
@@ -1,12 +1,24 @@
+from plone.autoform.interfaces import IFormFieldProvider
 from plone.dexterity.content import Item
 from plone.supermodel import model
+from recensio.plone import _
+from zope import schema
 from zope.interface import implementer
+from zope.interface import provider
 
 
+@provider(IFormFieldProvider)
 class IReviewMonograph(model.Schema):
-    """Marker interface and Dexterity Python Schema for ReviewMonograph"""
+    """Marker interface and Dexterity Python Schema for ReviewMonograph."""
+
+    # TODO
+    # schemata="reviewed_text"
+    translatedTitle = schema.TextLine(
+        title=_("Translated Title"),
+        required=False,
+    )
 
 
 @implementer(IReviewMonograph)
 class ReviewMonograph(Item):
-    """Content-type class for IReviewMonograph"""
+    """Content-type class for IReviewMonograph."""
