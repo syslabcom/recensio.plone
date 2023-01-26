c3bbfb762dee94b2ae9de892c865ad89a9062242
Johannes Raggam <thetetet@gmail.com>
2023-01-26 13:52:47 +0100

    Change get_no_pages to get_num_pages for better explaining what it does.

--- src/recensio/plone/browser/pageviewer.py
+++ src/recensio/plone/browser/pageviewer.py
@@ -4,7 +4,7 @@ from Products.Five.browser import BrowserView
 class Pageviewer(BrowserView):
     """Views a review's page images (generated from pdf)"""
 
-    def get_no_pages(self):
+    def get_num_pages(self):
         """Return the number of pages that are stored as images See
         get_page_image()"""
         pagePictures = getattr(self.context, "pagePictures", [])
@@ -12,7 +12,7 @@ class Pageviewer(BrowserView):
 
     def __call__(self):
         html = '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">\n<head></head>\n<body>\n'
-        for i in range(self.get_no_pages() + 1)[1:]:
+        for i in range(self.get_num_pages() + 1)[1:]:
             html = (
                 html
                 + '    <div>\n      <img src="'
