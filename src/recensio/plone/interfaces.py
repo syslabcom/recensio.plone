645431c877be5d6e91a60a0bf35c82322284bb7a
ale-rt <alessandro.pisa@gmail.com>
2023-01-27 09:28:45 +0100

    Register the parent getter adapter

--- src/recensio/plone/interfaces.py
+++ src/recensio/plone/interfaces.py
@@ -8,3 +8,25 @@ class IRecensioPloneLayer(IDefaultBrowserLayer):
 
 class IReview(Interface):
     """Marker interface for reviews and other select content types."""
+
+
+class IParentGetter(Interface):
+    """Get parent of a certain content type."""
+
+    def get_parent_object_of_type(meta_type):
+        """Return the object of a particular type which is the parent of the
+        current object."""
+
+    def get_title_from_parent_of_type(meta_type):
+        """Return the title of the first object of a particular type which is a
+        parent of the current object."""
+
+    def get_flag_with_override(field_name, override_value):
+        """Retrieves a boolean field value, allowing for overrides from a
+        parent.
+
+        If any object in the acquisition chain has the override_value
+        set for the field specified by field_name, then this value is
+        used. Only if no object in the chain has it set is the inverse
+        value used.
+        """
