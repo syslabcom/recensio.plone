4f14f453224e2fcd7af7d25f5ed7dc2e96b3c01c
Manuel Reinhardt <reinhardt@syslab.com>
2023-02-02 11:49:54 +0100

    Get rid of the too-clever alphabet index
    It only worked with solr and was a bit unstable. Let's see if we can get
    away with replacing it with something simple: Clicking “A” searches for
    “A*”.
    
    syslabcom/scrum#948

--- src/recensio/plone/browser/authorsearch.py
+++ src/recensio/plone/browser/authorsearch.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 from datetime import datetime
 from plone.memoize import instance
-from plone.memoize import ram
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.browser.navtree import getNavigationRoot
 from Products.CMFPlone.utils import safe_unicode
@@ -36,75 +35,12 @@ class AuthorSearchView(BrowserView, CrossPlatformMixin):
         self.request.set("disable_border", True)
         return self.template(self)
 
-    @ram.cache(_render_cachekey)
-    def get_b_start(self):
-        b_start = int(self.request.get("b_start", 0))
-        if b_start:
-            return b_start
-        else:
-            letter = self.request.get("letter")
-            if not letter:
-                return 0
-            # XXX this is way too clever
-            letter = letter.lower()
-            catalog = getToolByName(self.context, "portal_catalog")
-            query = {
-                "portal_type": "Person",
-                "b_start": 0,
-                "b_size": int(self.request.get("b_size", 30)),
-                "sort_on": "sortable_title",
-                "fl": "Title",
-            }
-            navigation_root = getNavigationRoot(self.context)
-            if self.request.get("use_navigation_root", True):
-                query["path"] = navigation_root
-            num_authors = len(catalog(query))
-            if num_authors == 0:
-                return 0
-            b_size = 30
-            query["b_size"] = b_size + 1
-            partition = [0, num_authors]
-            sample_idx = 0
-            while True:
-                sample_start = max(
-                    partition[0] + (partition[1] - partition[0]) / 2,
-                    0,
-                )
-                sample_start = int(sample_start / b_size) * b_size
-                query["b_start"] = max(sample_start - 1, 0)
-                sample = catalog(query)
-                sample_end = min(sample_start + b_size - 1, num_authors - 1)
-                if sample[sample_end]["Title"][0].lower() < letter:
-                    # last item in sample is before letter - move further towards end
-                    partition[0] = min(sample_end + 1, num_authors)
-                elif (
-                    sample_start == 0
-                    or sample[sample_start - 1]["Title"][0].lower() < letter
-                ):
-                    break
-                else:
-                    # first item in sample is our letter or after - move further towards beginning
-                    partition[1] = max(sample_start - 1, 0)
-
-                sample_idx += 1
-                if sample_idx >= 9:
-                    logger.warning("Bailing out to avoid infinite loop")
-                    break
-
-            b_start = sample_start
-            for idx in range(sample_start, sample_end):
-                if sample[idx]["Title"].lower().startswith(letter):
-                    b_start = idx
-                    break
-            b_start = int(b_start / b_size) * b_size
-        return b_start
-
     @property
     @instance.memoize
     def authors(self):
         catalog = getToolByName(self.context, "portal_catalog")
         author_string = safe_unicode(self.request.get("authors"))
-        b_start = self.get_b_start()
+        b_start = int(self.request.get("b_start", 0))
         b_size = int(self.request.get("b_size", 30))
         query = {
             "portal_type": "Person",
---
+++ src/recensio/plone/tests/test_authorsearch.py
@@ -0,0 +1,50 @@
+from plone import api
+from recensio.plone.interfaces import IRecensioPloneLayer
+from recensio.plone.testing import RECENSIO_PLONE_INTEGRATION_TESTING
+from zope.interface import alsoProvides
+from zope.interface import noLongerProvides
+
+import unittest
+
+
+class TestAuthorSearch(unittest.TestCase):
+    """"""
+
+    layer = RECENSIO_PLONE_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer["portal"]
+        self.request = self.layer["request"].clone()
+        alsoProvides(self.request, IRecensioPloneLayer)
+        if "gnd" not in self.portal:
+            with api.env.adopt_roles(["Manager"]):
+                api.content.create(
+                    type="Folder",
+                    container=self.portal,
+                    id="gnd",
+                    title="GND",
+                )
+
+        gnd_view = api.content.get_view(
+            name="gnd-view", context=self.portal, request=self.request
+        )
+        for data in [
+            {"lastname": "Kot\u0142owski", "firstname": "Tadeusz"},
+            {"lastname": "Benthin", "firstname": "Patrick"},
+            {"lastname": "Eimer", "firstname": "Claudio"},
+            {"lastname": "Eimer", "firstname": "Kathy"},
+        ]:
+            gnd_view.createPerson(**data)
+
+    def tearDown(self):
+        noLongerProvides(self.request, IRecensioPloneLayer)
+
+    def test_search(self):
+        self.request.form = {"authors": "Eimer"}
+        view = api.content.get_view(
+            context=self.portal, request=self.request, name="authorsearch"
+        )
+        result = view.authors
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0]["Title"], "Eimer, Claudio")
+        self.assertEqual(result[1]["Title"], "Eimer, Kathy")
