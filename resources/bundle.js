f241f82b795e65fb25787cf0fd029c41ef6d5582
Johannes Raggam <thetetet@gmail.com>
2023-02-02 09:44:21 +0100

    UI: Add custom Patternslib bundle.

---
+++ resources/bundle.js
@@ -0,0 +1,8 @@
+import registry from "@patternslib/patternslib/src/core/registry";
+
+import "@patternslib/patternslib/src/pat/collapsible/collapsible";
+import "@patternslib/patternslib/src/pat/gallery/gallery";
+import "@patternslib/patternslib/src/pat/switch/switch";
+import "@patternslib/patternslib/src/pat/toggle/toggle";
+
+registry.init();
