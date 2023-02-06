59cf8bebf5e23ae3584b04e9590c37f0fc81aea2
Manuel Reinhardt <reinhardt@syslab.com>
2023-02-06 14:34:19 +0100

    Add more catalog metadata (authors/editors etc.)

--- src/recensio/plone/adapter/indexer.py
+++ src/recensio/plone/adapter/indexer.py
@@ -16,3 +16,46 @@ def authorsUID(obj):
         if author_obj:
             uids.append(author_obj.UID())
     return uids
+
+
+def _listAuthors(obj, listEditors=False):
+    if not getattr(obj, "authors", None):
+        return []
+    retval = []
+    if getattr(obj, "curators", None):
+        for curator in obj.curators:
+            curator_obj = curator.to_object
+            if curator_obj.lastname or curator_obj.firstname:
+                retval.append(f"{curator_obj.lastname}, {curator_obj.firstname}")
+    if listEditors and getattr(obj, "editorial", None):
+        for editor in obj.editorial:
+            editor_obj = editor.to_object
+            if editor_obj.lastname or editor_obj.firstname:
+                retval.append(f"{editor_obj.lastname}, {editor_obj.firstname}")
+    for author in obj.authors:
+        author_obj = author.to_object
+        if author_obj.lastname or author_obj.firstname:
+            retval.append(f"{author_obj.lastname}, {author_obj.firstname}")
+    return retval
+
+
+@indexer(IReview)
+def listAuthors(obj):
+    return _listAuthors(obj, listEditors=False)
+
+
+@indexer(IReview)
+def listAuthorsAndEditors(obj):
+    return _listAuthors(obj, listEditors=True)
+
+
+@indexer(IReview)
+def listReviewAuthors(obj):
+    if not getattr(obj, "reviewAuthors", None):
+        return []
+    retval = []
+    for author in obj.reviewAuthors:
+        author_obj = author.to_object
+        if author_obj and author_obj.lastname or author_obj.firstname:
+            retval.append(f"{author_obj.lastname}, {author_obj.firstname}")
+    return retval
