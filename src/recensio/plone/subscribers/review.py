8dd1d6be40dc64315dd845c7e4677125cc8630e3
Cillian de Roiste <deroiste@syslab.com>
2023-05-15 10:05:43 +0100

    Fix pdf generation with valid html5
    Thanks to ale
    
    Refs: https://github.com/syslabcom/scrum/issues/877

--- src/recensio/plone/subscribers/review.py
+++ src/recensio/plone/subscribers/review.py
@@ -22,9 +22,9 @@ logger = getLogger(__name__)
 
 HTML_TEMPLATE = Template(
     """
-<?xml version="1.0"?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head>
 <body>$body</body>
 </html>
 """.strip()
