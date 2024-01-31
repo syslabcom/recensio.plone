db3d7c21083caafda484da3c31c681b5c2aba0b9
Manuel Reinhardt <reinhardt@syslab.com>
2024-01-31 12:19:18 +0100

    Use default page in correct language for publications if possible
    syslabcom/scrum#889

--- src/recensio/plone/browser/publications.py
+++ src/recensio/plone/browser/publications.py
@@ -1,12 +1,41 @@
 from DateTime import DateTime
 from plone import api
 from plone.memoize import ram
+from Products.CMFPlone.browser.defaultpage import DefaultPage
 from Products.Five.browser import BrowserView
 from recensio.plone.browser.canonical import CanonicalURLHelper
 
 
 def _render_cachekey(method, self, brain, lang):
     return (brain.getPath(), lang, DateTime().dayOfYear())
+
+
+class PublicationDefaultPage(DefaultPage):
+    """Translated default page"""
+
+    def getDefaultPage(self):
+        """Get the default page, if possible in the correct language.
+        We expect the default page to have no language suffix (e.g. 'index').
+        Translations should have a language suffix (e.g. 'index-en').
+        If this fails we check the language attribute of all contained documents.
+        If there is only one document with the right language we return that.
+        """
+        default_page = super().getDefaultPage()
+        language = api.portal.get_current_language()
+        defob = self.context.get(default_page)
+        if not defob or defob.Language() == language:
+            return default_page
+        documents = self.context.listFolderContents(dict(portal_type="Document"))
+        id_match = [
+            item for item in documents if item.getId() == f"{default_page}-{language}"
+        ]
+        if len(id_match) == 1:
+            return id_match[0].getId()
+
+        language_match = [item for item in documents if item.Language() == language]
+        if len(language_match) == 1:
+            return language_match[0].getId()
+        return default_page
 
 
 class PublicationsView(BrowserView, CanonicalURLHelper):
