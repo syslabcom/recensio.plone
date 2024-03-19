0efceaecdd007da0736bf947b5f032aaa65b33be
Johannes Raggam <thetetet@gmail.com>
2024-03-19 18:47:45 +0100

    import page pictures: Adapt script to export_page_pictures view from recensio.policy.

--- src/recensio/plone/migration/import_pagePictures.py
+++ src/recensio/plone/migration/import_pagePictures.py
@@ -67,6 +67,15 @@ class ImportPagePicturesForm(form.Form):
                         pagePictures.append(
                             NamedBlobImage(data=f.read(), filename=f"{blob_idx+1}.gif")
                         )
+                elif isinstance(blob_path, str):
+                    # Looks like, when export_page_pictures was called TTW, the
+                    # whole blob path was stored as string. No need to use
+                    # COLLECTIVE_EXPORTIMPORT_BLOB_HOME in that case.
+                    with open(blob_path, "rb") as f:
+                        pagePictures.append(
+                            NamedBlobImage(data=f.read(), filename=f"{blob_idx+1}.gif")
+                        )
+
             obj.pagePictures = pagePictures
 
         self.status = _("File uploaded")
