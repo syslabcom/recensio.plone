5c7332e0a80d8e7a673fcf92110237184e4599a0
Guido A.J. Stevens <guido.stevens@cosent.nl>
2025-09-25 12:24:28 +0200

    Move solr reindex to a separate upgrade step

--- src/recensio/plone/upgrades/v2/20250924111704_install_collective_solr/upgrade.py
+++ src/recensio/plone/upgrades/v2/20250924111704_install_collective_solr/upgrade.py
@@ -1,10 +1,4 @@
 from ftw.upgrade import UpgradeStep
-from logging import getLogger
-from plone import api
-from transaction import commit
-
-
-log = getLogger(__name__)
 
 
 class InstallCollectiveSolr(UpgradeStep):
@@ -12,12 +6,3 @@ class InstallCollectiveSolr(UpgradeStep):
 
     def __call__(self):
         self.install_upgrade_profile()
-
-        # ensure solr activation is persisted before performing the reindex
-        commit()
-
-        portal = api.portal.get()
-        maintenance = api.content.get_view("solr-maintenance", portal)
-        log.info("Reindexing to Solr. This will take a while.")
-        maintenance.reindex()
-        log.info("Solr reindexing done.")
