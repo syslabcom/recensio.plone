b7f090b46c3b9674777f5482dcc9b196c7afbdcc
Guido A.J. Stevens <guido.stevens@cosent.nl>
2025-09-25 12:34:58 +0200

    Load collective.solr zcml in zcml

--- src/recensio/plone/testing.py
+++ src/recensio/plone/testing.py
@@ -4,7 +4,6 @@ from plone.app.testing import applyProfile
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import PloneSandboxLayer
 
-import collective.solr
 import recensio.plone
 
 
@@ -12,7 +11,6 @@ class RecensioPloneLayer(PloneSandboxLayer):
     defaultBases = (PLONE_APP_CONTENTTYPES_FIXTURE,)
 
     def setUpZope(self, app, configurationContext):
-        self.loadZCML(package=collective.solr)
         self.loadZCML(package=recensio.plone)
 
     def setUpPloneSite(self, portal):
